61d78573af4064e825c3d4636947bb941795de38
suganmohan <71695126+suganneshan@users.noreply.github.com>
2021-04-03 21:56:03 +0530

    Update side.js

--- side.js
+++ side.js
@@ -28,104 +28,92 @@ function test(){
         });
     });
 }
-// $(document).ready(function(){
-//     setTimeout(function() { test(); });
-// });
-// $(window).on('resize',function(){
-//     setTimeout(function(){ test(); },500);
-// });
-// $(".navbar-toggler").click(function(){
-//     setTimeout(function(){ test(); });
-// });
-
-// /*callback fuction */
-// $(document).ready(function(){
-//     $("#submit").click(function(){
-//         var name = $("#name").val();
-//         var email = $("#email").val();
-//         var phone = $("#phone").val();
+$(document).ready(function(){
+    setTimeout(function() { test(); });
+});
+$(window).on('resize',function(){
+    setTimeout(function(){ test(); },500);
+});
+$(".navbar-toggler").click(function(){
+    setTimeout(function(){ test(); });
+});
+/*callback fuction */
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var valid
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
         
-//         if(name.length == "")
-//         {
-//             Swal.fire({
-//                 position: 'center',
-//                 icon: 'warning',
-//                 title: 'Please  Fill Your Name',
-//                 showConfirmButton:false,
-//                 timer: 2500,
-//                 showClass: {
-//                     popup: 'animate__animated animate__fadeInDown'
-//                   },
-//                   hideClass: {
-//                     popup: 'animate__animated animate__fadeOutUp'
-//                   }
-//               });
-//             $("#p1").text("Please Enter Your Name")
-//             $("#name").focus();            
-//             return false;
-//         }
+        if(name.length == "")
+        {
+         Swal.fire({
+         icon: 'warning',
+         title: 'Please Enter your name',
+         showConfirmButton: false,
+         timer: 1500,
+         showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+          }
+         })
+            $("#name").focus();            
+            return false;
+        }
         
-//         else if(email.length == "")
-//         {
+        else if(email.length == "")
+        {
             
-//               Swal.fire({
-//                 position: 'center',
-//                 icon: 'warning',
-//                 title: 'Please Fill your Email',
-//                 showConfirmButton:false,
-//                 timer: 2500,
-//                 showClass: {
-//                     popup: 'animate__animated animate__fadeInDown'
-//                   },
-//                   hideClass: {
-//                     popup: 'animate__animated animate__fadeOutUp'
-//                   }
-//               });
-//              $("#p2").text("enter your  Email Id")
-//             $("#email").focus();
-//             return false;
-//         }
+            Swal.fire({
+                icon: 'warning',
+                title: 'Please Enter your Email',
+                showConfirmButton: false,
+                timer: 1500,
+                showClass: {
+                   popup: 'animate__animated animate__fadeInDown'
+                 },
+                 hideClass: {
+                   popup: 'animate__animated animate__fadeOutUp'
+                 }
+       })
+            $("#email").focus();
+            return false;
+        }
         
-//         else if(phone.length == "")
-//         {
-//             Swal.fire({
-//                 position: 'center',
-//                 icon: 'warning',
-//                 title: 'Please fill your Phone Number',
-//                 showConfirmButton:false,
-//                 timer: 2500,
-//                 showClass: {
-//                     popup: 'animate__animated animate__fadeInDown'
-//                   },
-//                   hideClass: {
-//                     popup: 'animate__animated animate__fadeOutUp'
-//                   }
-//               })
-//             $("#p3").text("Enter your Phone Number")
-//             $("#phone").focus();
-//             return false;
-//         }
-//         else 
-//         {   
-//             Swal.fire({
-//                 title: 'Do you want to save the changes?',
-//                 showDenyButton: true,
-//                 showCancelButton: true,
-//                 confirmButtonText: `Call Back`,
-//                 denyButtonText: `Don't Call Back`,
-//               }).then((result) => {
-//                 /* Read more about isConfirmed, isDenied below */
-//                 if (result.isConfirmed) {
-//                   Swal.fire('Call within Today!', '', 'success')
-
-//                 } else if (result.isDenied) {
-//                   Swal.fire(' Your callBack is  canceled', '', 'info')
-//                 }
-//               })
-//               return false;
-//         }
-//     })
-// })
+        else if(phone.length == "")
+        {
+  
+            Swal.fire({
+                icon: 'warning',
+                title: 'Please Enter your Phone',
+                showConfirmButton: false,
+                timer: 1500,
+                showClass: {
+                   popup: 'animate__animated animate__fadeInDown'
+                 },
+                 hideClass: {
+                   popup: 'animate__animated animate__fadeOutUp'
+                 }
+       })
+            $("#phone").focus();
+            return false;
+        }
+        else 
+        {   
+           var con =confrim("Are You Done?");
+               if(con == true)
+               {
+                 alert("Welcome to our website");
+                 return true;
+               }
+               else{
+                 return false;
+               }
+        }
+    })
+})
 // !(function($) {
 //     // Preloader
 //     $(window).on('load', function() {
